b1889576f4a963e92a81940cf16637c92eacd16b
unknown <rcadmin@LPXMN112.rcoffice.ringcentral.com>
2022-11-27 03:42:03 +0800

    test

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,1 @@
-declare interface IStatus {
-  signed: boolean,
-  message: string
-}
-
-export function isSignedVerbose(filePath: string): Promise<IStatus>;
-export function isSigned(filePath: string): Promise<boolean>;
-export function trustStatus(filePath: string): Promise<string>;
+export function verifySignatureByPublishName(filePath: string, publishName: string): Promise<boolean>;
