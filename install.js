59a2ddf2a89f5f8804ec474af20290c66e92a3bc
payne.fu <payne.fu@ringcentral.com>
2022-12-23 14:37:40 +0800

    update install.js

--- install.js
+++ install.js
@@ -2,7 +2,7 @@ const os = require("os");
 const { spawnSync } = require("child_process");
 
 if (os.platform() === "win32") {
-  spawnSync("npm", ["run", "native_build"], {
+  spawnSync("npm.cmd", ["run", "native_build"], {
     input: "win32 detected. Build native module.",
     stdio: "inherit",
   });
