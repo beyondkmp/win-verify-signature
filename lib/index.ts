5eaa005c7a18ae22622d248a429c3d2c9b27135a
payne.fu <payne.fu@ringcentral.com>
2023-01-07 18:06:58 +0800

    refactor

---
+++ lib/index.ts
@@ -0,0 +1,17 @@
+type NativeModule = {
+  is24hoursTimeFormat: () => boolean
+}
+
+// The native binary will be loaded lazily to avoid any possible crash at start
+// time, which are harder to trace.
+let _nativeModule: NativeModule | undefined = undefined
+
+function getNativeModule() {
+  _nativeModule = require('bindings')('check-24-hours-time.node')
+  return _nativeModule
+}
+
+export function is24hoursTimeFormat(): boolean {
+  const result = getNativeModule()?.is24hoursTimeFormat()
+  return !!result
+}
