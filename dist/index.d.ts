22e385528afd0a7bba7a1fc51601a2d276d40d56
payne.fu <payne.fu@ringcentral.com>
2023-01-09 20:06:29 +0800

    delete dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface ISignStatus {
-    signed: boolean;
-    message: string;
-    subject?: string;
-}
-export declare function verifySignatureByPublishName(filePath: string, publishNames: string[]): ISignStatus;
-export {};
